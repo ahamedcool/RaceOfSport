0978bfee6c29f6335c74bb364cff63fa51622e2c
woohyunjin06 <woohyunjin06@naver.com>
2018-06-02 21:10:26 +0900

    Initial commit

---
+++ app/src/main/java/com/highthon/raceofsport/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.highthon.raceofsport
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.menu_need -> startActivity<NeedActivity>()
+        }
+        return false
+    }
+}
